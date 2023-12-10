5d4b8edb7ca515e17c612712a5738159d9f5d6f9
developer-father <W@email>
2023-12-10 13:58:43 +0800

    美化显示和对窗口滑轮问答时不会置于顶部进行修改，代码部分进行修改删除部分冗余部分，对\n处理利用pre标签特性，对其进行转化为字符串并替换为合适的格式，最后进行highlight点亮代码（这一块有点问题待更新！！！），最后进行markdown格式输出结果

--- js/index.js
+++ js/index.js
@@ -1,27 +1,22 @@
 let isRequestInProgress = false;
+let chatWindow = document.getElementById("conversationContainer");
 let conversationHistory = [{'role': 'system', 'content': '你是一个无所不能的帮手' }];
 var count = 0;
 let center_to_out = ''
-// const options = {
-//     gfm: true,
-//     breaks: true,
-//     smartLists: true,
-//     highlight: function(code) {
-//       return require('highlight.js').highlightAuto(code).value;
-//     }
-//   };
+const options = {
+    gfm: true
+  };
 function updateChatWindow(content, id) {
-    console.log(content);
     const existingDiv = document.getElementById(id);
-    center_to_out += content.replace(/"/g, '');
-    let push_origin = center_to_out;
+    center_to_out += content.replace(/^"|"$/g, '');
+    // let push_origin = center_to_out;
     if (existingDiv) {
-        existingDiv.innerHTML = (push_origin).replace(/\\n/g, "<br/>").replace(/ /g, "&nbsp;");
+        existingDiv.innerHTML = `<pre>${marked.parse(center_to_out.replace(/\\n/g, '\n'),options)}</pre>`;//.replace(/\\n/g, "<br/>").replace(/ /g, "&nbsp;")
         chatWindow.scrollTop = chatWindow.scrollHeight;
     } else {
         const userChat = document.createElement('div');
         userChat.classList.add('chat', 'chat-end');
-        userChat.innerHTML = `<div class="chat-bubble" id=${count}><strong>AI:</strong> ${push_origin}</div>`;
+        userChat.innerHTML = `<div class="chat-bubble" id=${count}><strong>AI:</strong>${center_to_out}</div>`;
         const conversationContainer = document.getElementById('conversationContainer');
         conversationContainer.appendChild(userChat);
     }
@@ -55,14 +50,8 @@ async function makeOpenAIRequest(model, messages) {
         eventSource.addEventListener('message', function(event) {
             const content = event.data;
             if (content === '{"done": true}') {
-                // var myDivContent = document.getElementById(count).innerHTML;
-                // var tempDiv = document.createElement('div');
-                // tempDiv.innerHTML = myDivContent;
-                // var pContent = tempDiv.getElementsByTagName('p')[0].innerHTML;
-                // // 将字符串中的 \\n 替换为 <br/>，空白部分用 &nbsp; 代替
-                // codeStr = pContent.replace(/\\n/g, "<br/>").replace(/ /g, "&nbsp;");
-                // document.getElementById(count).innerHTML = marked.prase(codeStr);
-                // // myDivContent = marked.parse(pContent);
+                // end_to_ = document.getElementById(`${count}`);
+                // end_to_.innerHTML = `<pre>${marked.parse(center_to_out.replace(/\\n/g, '\n'))}</pre>`
                 console.log('end');
             } else {
                 const existingLoadingChat = document.getElementById('loading');
@@ -87,6 +76,7 @@ async function makeOpenAIRequest(model, messages) {
 
 function handleKeyPress(event) {
     if (event.key === 'Enter' || event.type === 'click') {
+        chatWindow.scrollTop = chatWindow.scrollHeight;
         const modelSelect = document.getElementById('modelSelect');
         const selectedModel = modelSelect.value;
         const promptInput = document.getElementById('promptInput');
