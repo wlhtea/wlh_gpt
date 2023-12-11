2a5020eb9e440f09965e6a00102ceff3b363df14
developer-father <W@email>
2023-12-11 14:59:53 +0800

    修复无法上网功能

--- js/index.js
+++ js/index.js
@@ -8,6 +8,34 @@ const options = {
     smartypants: true,
     smartLists: true
   };
+
+
+function showAlert(isSuccess, message) {
+    var toastContainer = document.getElementById('toastContainer');
+    toastContainer.innerHTML = '';
+
+    var toast = document.createElement('div');
+    toast.className = 'toast';
+
+    if (isSuccess) {
+        toast.classList.add('alert-success');
+    } else {
+        toast.classList.add('alert-info');
+    }
+
+    toast.textContent = message;
+    toastContainer.appendChild(toast);
+    setTimeout(function() {
+        toast.style.opacity = 1;
+    }, 10);
+    setTimeout(function() {
+        toast.style.opacity = 0;
+    }, 2000);
+    setTimeout(function() {
+        toastContainer.innerHTML = '';
+    }, 2500);
+}
+
 function updateChatWindow(content, id) {
     const existingDiv = document.getElementById(id);
     center_to_out += content.replace(/^"|"$/g, '').replace(/\\"/g,'"');
@@ -28,7 +56,7 @@ function updateChatWindow(content, id) {
 
 async function makeOpenAIRequest(model, messages) {
     if (isRequestInProgress) {
-        showAlert('等待上一个请求完成！');
+        showAlert(false,'等待上一个请求完成！');
         return;
     }
 
@@ -67,7 +95,7 @@ async function makeOpenAIRequest(model, messages) {
             eventSource.close();
         });
     } catch (error) {
-        showAlert(error);
+        showAlert(false,error);
     } finally {
         center_to_out = '';
         isRequestInProgress = false;
@@ -77,28 +105,43 @@ async function makeOpenAIRequest(model, messages) {
 
 function handleKeyPress(event) {
     if (event.key === 'Enter' || event.type === 'click') {
-        chatWindow.scrollTop = chatWindow.scrollHeight;
         const modelSelect = document.getElementById('modelSelect');
         const selectedModel = modelSelect.value;
         const promptInput = document.getElementById('promptInput');
         const userPrompt = promptInput.value.trim();
         conversationHistory.push({ role: 'user', content: userPrompt });
         promptInput.value = '';
+        chatWindow.scrollTop = chatWindow.scrollHeight;
         if (selectedModel == 'dall-e-3') {
             // makeOpenAIRequest(selectedModel, conversationHistory);
             // count += 1;
-            showAlert('厚米 求你别搞我！！');
+            showAlert(false,'厚米 求你别搞我！！');
         } else {
-            let net_ = "";
-            var checkbox = document.getElementById('myCheckbox');
-            if (checkbox.checked) {
-                net_ = "net-";
-            } else {
-                net_ = net_;
-            }
-            let result_model = net_.concat("", selectedModel);
             makeOpenAIRequest(result_model, conversationHistory);
             count += 1;
         }
     }
+}
+var myCheckboxfy = document.getElementById('myCheckboxfy');
+myCheckboxfy.addEventListener('change', transale);
+function transale(){
+    if (myCheckboxfy.checked){
+        showAlert(true,'开启翻译模式');
+        conversationHistory = [{role:'system',content:'## 角色 & 任务 ### 任务 我希望你以一个专业翻译团队的身份，协助完成从英文到中文的翻译任务。 ### 角色 对于每个翻译任务，我将扮演三个专家角色，分别负责翻译、校对与润色工作： 1. 翻译专家：具有20年翻译经验，精通中英双语，并拥有丰富的跨学科知识。此阶段的目标是提供一份既忠实于原文，又在中文中读起来流畅自然的初稿。在翻译时，特别注重保持原文的风格和语调。 2. 资深校对编辑：拥有20年专业编辑经验，中文系毕业，对中文语法、用词有精准把握。在此阶段，您需要对翻译稿进行深度校对，包括语法、用词、风格的校正，确保翻译的准确性和易读性，进而输出第二版翻译稿。 3. 润色专家：作为一位拥有20年写作经验的获奖作家，擅长各种风格流派的写作。在此阶段，您需要在校对编辑提供的稿件基础上，进行风格上的润色，提高文本的文学美感，同时保持原文的专业性和准确性。例如，润色诗歌时应使用更优美、富有意境的语言；润色科技类文章时则应维持其专业性和准确性。 ## 工作流程 ### 1. 翻译阶段 参与人：翻译专家 输出物：翻译稿件 任务：提供忠实原文且流畅的中文初稿。 ### 2. 校对阶段 参与人：资深校对编辑 输出物：校对过的翻译稿件 任务：深度校对初稿，保证准确性和易读性。 ### 3. 润色阶段 参与人：润色专家 输出物：润色过后的最终翻译稿 任务：提升文本的风格美感，同时保持专业性和准确性. 最终输出内容：翻译的最终内容，其余信息无需输出'}]
+    } else{
+        conversationHistory = [{'role': 'system', 'content': '你是一个无所不能的帮手' }];
+    }
+}
+let net_ = "";
+let result_model = 'gpt-3.5-turbo';
+var checkbox = document.getElementById('myCheckbox');
+checkbox.addEventListener('change', checked_to);
+function checked_to(){
+    if (checkbox.checked) {
+        showAlert(true,'开启联网模式');
+        net_ = "net-";
+    } else {
+        net_ = net_;
+    }
+    result_model = net_.concat("", result_model);
 }
