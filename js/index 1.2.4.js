f06d96dc26e8642ad7dbcc4adcffe6c5b442bd38
developer-father <W@email>
2023-12-12 14:26:45 +0800

    修复上传问题 提示词覆盖问题 存在问题无法第一次发送就上传成功（待解决）

--- js/index 1.2.4.js
+++ js/index 1.2.4.js
@@ -13,18 +13,17 @@ const options = {
 function uploadPDF() {
     var pdfInput = document.getElementById('pdfInput');
     var file = pdfInput.files[0];
-
     if (!file) {
         return;
     }
     var formData = new FormData();
     formData.append('file', file);
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://127.0.0.1:5000/extract_text');
+    xhr.open('POST', 'http://8.138.104.244:5000/extract_text');
     xhr.onload = function () {
         if (xhr.status === 200) {
             var response = JSON.parse(xhr.responseText);
-            conversationHistory.push({ role: 'user', content: response.result})
+            conversationHistory.push({ role: 'user', content: response.result});
         } else {
             showAlert(false,`上传失败${error}`);
         }
@@ -77,7 +76,7 @@ function updateChatWindow(content, id) {
     console.log(center_to_out);
     // let push_origin = center_to_out;
     if (existingDiv) {
-        existingDiv.innerHTML = `<pre>${marked.parse(center_to_out.replace(/\\n/g, '\n'),options)}</pre>`;//.replace(/\\n/g, "<br/>").replace(/ /g, "&nbsp;")
+        existingDiv.innerHTML = `<pre>${marked.parse(center_to_out.replace(/\\n/g, '\n').replace(/\n\n\n/g, '\n'),options)}</pre>`;//.replace(/\\n/g, "<br/>").replace(/ /g, "&nbsp;")
         chatWindow.scrollTop = chatWindow.scrollHeight;
     } else {
         const userChat = document.createElement('div');
@@ -153,8 +152,8 @@ async function makeOpenAIRequest(model, messages) {
 
 
 function handleKeyPress(event) {
-    console.log(input_values_id.scrollHeight);
     if ((event.key === 'Enter' && !event.shiftKey) || event.type === 'click' ) {
+        handleUpload();
         event.preventDefault();
         const modelSelect = document.getElementById('modelSelect');
         const selectedModel = modelSelect.value;
@@ -164,13 +163,13 @@ function handleKeyPress(event) {
             showAlert(false, '请输入内容后再发送！');
             return;
         }
-        handleUpload();
         conversationHistory.push({ role: 'user', content: userPrompt });
         promptInput.value = '';
         chatWindow.scrollTop = chatWindow.scrollHeight;
         if (selectedModel == 'dall-e-3') {
             showAlert(false,'厚米 求你别搞我！！');
         } else {
+            console.log(conversationHistory);
             makeOpenAIRequest(selectedModel, conversationHistory);
             count += 1;
         }
@@ -191,8 +190,9 @@ myCheckboxfy.addEventListener('change', transale);
 function transale(){
     if (myCheckboxfy.checked){
         showAlert(true,'开启翻译模式');
-        conversationHistory = [{role:'system',content:'## 角色 & 任务 ### 任务 我希望你以一个专业翻译团队的身份，协助完成从英文到中文的翻译任务。 ### 角色 对于每个翻译任务，我将扮演三个专家角色，分别负责翻译、校对与润色工作： 1. 翻译专家：具有20年翻译经验，精通中英双语，并拥有丰富的跨学科知识。此阶段的目标是提供一份既忠实于原文，又在中文中读起来流畅自然的初稿。在翻译时，特别注重保持原文的风格和语调。 2. 资深校对编辑：拥有20年专业编辑经验，中文系毕业，对中文语法、用词有精准把握。在此阶段，您需要对翻译稿进行深度校对，包括语法、用词、风格的校正，确保翻译的准确性和易读性，进而输出第二版翻译稿。 3. 润色专家：作为一位拥有20年写作经验的获奖作家，擅长各种风格流派的写作。在此阶段，您需要在校对编辑提供的稿件基础上，进行风格上的润色，提高文本的文学美感，同时保持原文的专业性和准确性。例如，润色诗歌时应使用更优美、富有意境的语言；润色科技类文章时则应维持其专业性和准确性。 ## 工作流程 ### 1. 翻译阶段 参与人：翻译专家 输出物：翻译稿件 任务：提供忠实原文且流畅的中文初稿。 ### 2. 校对阶段 参与人：资深校对编辑 输出物：校对过的翻译稿件 任务：深度校对初稿，保证准确性和易读性。 ### 3. 润色阶段 参与人：润色专家 输出物：润色过后的最终翻译稿 任务：提升文本的风格美感，同时保持专业性和准确性. 最终输出内容：翻译的最终内容，其余信息无需输出'}]
+        conversationHistory.push({role:'system',content:'## 角色 & 任务 ### 任务 我希望你以一个专业翻译团队的身份，协助完成从英文到中文的翻译任务。 ### 角色 对于每个翻译任务，我将扮演三个专家角色，分别负责翻译、校对与润色工作： 1. 翻译专家：具有20年翻译经验，精通中英双语，并拥有丰富的跨学科知识。此阶段的目标是提供一份既忠实于原文，又在中文中读起来流畅自然的初稿。在翻译时，特别注重保持原文的风格和语调。 2. 资深校对编辑：拥有20年专业编辑经验，中文系毕业，对中文语法、用词有精准把握。在此阶段，您需要对翻译稿进行深度校对，包括语法、用词、风格的校正，确保翻译的准确性和易读性，进而输出第二版翻译稿。 3. 润色专家：作为一位拥有20年写作经验的获奖作家，擅长各种风格流派的写作。在此阶段，您需要在校对编辑提供的稿件基础上，进行风格上的润色，提高文本的文学美感，同时保持原文的专业性和准确性。例如，润色诗歌时应使用更优美、富有意境的语言；润色科技类文章时则应维持其专业性和准确性。 ## 工作流程 ### 1. 翻译阶段 参与人：翻译专家 输出物：翻译稿件 任务：提供忠实原文且流畅的中文初稿。 ### 2. 校对阶段 参与人：资深校对编辑 输出物：校对过的翻译稿件 任务：深度校对初稿，保证准确性和易读性。 ### 3. 润色阶段 参与人：润色专家 输出物：润色过后的最终翻译稿 任务：提升文本的风格美感，同时保持专业性和准确性. 最终输出内容：翻译的最终内容，其余信息无需输出'})
     } else{
         conversationHistory = [{'role': 'system', 'content': '你是一个无所不能的帮手' }];
     }
 }
+
