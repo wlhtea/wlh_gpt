3d05b18376a1f49adb32346b835eeb215db64480
wlhtea <115779315+wlhtea@users.noreply.github.com>
2024-01-17 15:43:52 +0800

    Update python_token_ai.py

--- python_token_ai.py
+++ python_token_ai.py
@@ -2,7 +2,6 @@ import tiktoken
 import openai
 
 
-openai.api_key = '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'
 def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
     """Return the number of tokens used by a list of messages."""
     try:
